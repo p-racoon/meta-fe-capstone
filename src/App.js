e70f84f43c290e3078137c7b51288d8770e0c17b
Prasham Ashesh <prashamashesh@pranshis-MacBook-Pro.local>
2024-07-29 02:15:49 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import Header from './components/Header/Header';
+import IntroBanner from './components/IntroBanner/IntroBanner';
+import Specials from './components/Specials/Specials';
+import Footer from './components/Footer/Footer';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Testimonials from './components/Testimonials/Testimonials';
+import About from './components/About/About';
+
 
 function App() {
   return (
-    <div className="App">
-      Some text
-    </div>
+    <>
+      <Header/>
+      <main>
+      <IntroBanner/>
+      <Specials/>
+      <Testimonials/>
+      <About/>
+      </main>
+      <Footer/>
+    </>
   );
 }
 
